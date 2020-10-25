1d151491ed9eb501bb6706ab80bfc8dba0cc3fb8
Lime <mtimoshin99@gmail.com>
2020-10-25 22:30:11 +0300

    feat: added state controll for services

--- index.js
+++ index.js
@@ -19,14 +19,16 @@ export default class extends ServiceTransformer {
   async load(moduleName) {
     const foundModule = this.getModule(moduleName)
     if (foundModule) {
-      if (foundModule.type === 'dynamic') {
-        const stringContext = await Container.createNativeModule(foundModule)
-        this.addService(foundModule, stringContext)
-      }
-      if(foundModule.type === 'static') {
-        const scriptContext = Container.createScriptModule(foundModule)
-        this.moduleTarget.appendChild(scriptContext)
-        this.addService(foundModule, "", scriptContext)
+      if (!this.existService(foundModule)) {
+        if (foundModule.type === 'dynamic') {
+          const stringContext = await Container.createNativeModule(foundModule)
+          this.addService(foundModule, stringContext)
+        }
+        if(foundModule.type === 'static') {
+          const scriptContext = Container.createScriptModule(foundModule)
+          this.moduleTarget.appendChild(scriptContext)
+          this.addService(foundModule, "", scriptContext)
+        }
       }
     }
     return this.getService(moduleName) ?? null
