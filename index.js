8836ed697e6ba9bfeffd56a2d33479fb19e7ba53
Lime <mtimoshin99@gmail.com>
2020-10-25 23:21:47 +0300

    refactor: change release arch (version fix)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import ServiceTransformer from './lib/ServiceTransformer'
+import ServiceState from './lib/ServiceState'
 import Container from './lib/Container'
 
 export default class extends ServiceTransformer {
@@ -8,7 +9,6 @@ export default class extends ServiceTransformer {
    */
   constructor(modules) {
     super(modules);
-    this.container = new Container()
   }
   
   /**
@@ -19,18 +19,22 @@ export default class extends ServiceTransformer {
   async load(moduleName) {
     const foundModule = this.getModule(moduleName)
     if (foundModule) {
-      if (!this.existService(foundModule)) {
-        if (foundModule.type === 'dynamic') {
-          const stringContext = await Container.createNativeModule(foundModule)
-          this.addService(foundModule, stringContext)
-        }
-        if(foundModule.type === 'static') {
-          const scriptContext = Container.createScriptModule(foundModule)
-          this.moduleTarget.appendChild(scriptContext)
-          this.addService(foundModule, "", scriptContext)
+      if (!ServiceState.isProcess(foundModule.name)) {
+        ServiceState.addProcess(foundModule.name)
+        if (!this.existService(foundModule)) {
+          if (foundModule.type === 'dynamic') {
+            const stringContext = await Container.createNativeModule(foundModule)
+            this.addService(foundModule, stringContext)
+          }
+          if(foundModule.type === 'static') {
+            const scriptContext = Container.createScriptModule(foundModule)
+            this.moduleTarget.appendChild(scriptContext)
+            this.addService(foundModule, "", scriptContext)
+          }
+          ServiceState.deleteProcess(foundModule.name)
         }
       }
     }
-    return this.getService(moduleName) ?? null
+    return this.getService(moduleName)
   }
 }
